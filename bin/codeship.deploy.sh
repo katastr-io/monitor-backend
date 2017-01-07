785244a484eba3ed48a9f53a4644b63606ec77a2
Michal Zimmermann <zimmicz@gmail.com>
2017-01-07 18:53:04 +0100

    Silence deployment rsync

--- bin/codeship.deploy.sh
+++ bin/codeship.deploy.sh
@@ -6,5 +6,5 @@ cd ~/clone
 
 npm install && \
 npm run build && \
-rsync -avz ~/clone/dist/* mzimmer1@193.85.199.37:${HOMEDIR}/monitor-backend-new && \
+rsync -aqz ~/clone/dist/* mzimmer1@193.85.199.37:${HOMEDIR}/monitor-backend-new && \
 ssh mzimmer1@193.85.199.37 "rm -rf ${HOMEDIR}/monitor-backend/* && mv ${HOMEDIR}/monitor-backend-new/* ${HOMEDIR}/monitor-backend && rm -rf ${HOMEDIR}/monitor-backend-new"
