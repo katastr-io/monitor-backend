0687cd7636357f1b09789713a478ee163dcfe257
Michal Zimmermann <zimmicz@gmail.com>
2017-01-08 12:44:20 +0100

    Fix database connection

--- bin/codeship.deploy.sh
+++ bin/codeship.deploy.sh
@@ -6,5 +6,6 @@ cd ~/clone
 
 npm install && \
 npm run build && \
+sed -i 's/user: "cadastre"/user: "c_install"/g' dist/src/config.js && \
 rsync -aqz ~/clone/dist/* mzimmer1@193.85.199.37:${HOMEDIR}/monitor-backend-new && \
 ssh mzimmer1@193.85.199.37 "rm -rf ${HOMEDIR}/monitor-backend/* && mv ${HOMEDIR}/monitor-backend-new/* ${HOMEDIR}/monitor-backend && rm -rf ${HOMEDIR}/monitor-backend-new"
