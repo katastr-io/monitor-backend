935291b44eab5e7079babb1e4870a14ddbd9063d
Michal Zimmermann <zimmicz@gmail.com>
2017-01-15 15:28:39 +0100

    Use katastrio everywhere

--- bin/codeship.deploy.sh
+++ bin/codeship.deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-HOMEDIR="/home/mzimmer1/katastralni.cz"
+HOMEDIR="/opt/katastr.io"
 
 cd ~/clone
 
 npm install && \
 npm run build && \
-sed -i 's/user: "cadastre"/user: "c_install"/g' dist/src/config.js && \
 rsync -aqz ~/clone/dist/* mzimmer1@193.85.199.37:${HOMEDIR}/monitor-backend-new && \
 ssh mzimmer1@193.85.199.37 "rm -rf ${HOMEDIR}/monitor-backend/* && mv ${HOMEDIR}/monitor-backend-new/* ${HOMEDIR}/monitor-backend && rm -rf ${HOMEDIR}/monitor-backend-new"
--- src/tests/bin/codeship.sh
+++ src/tests/bin/codeship.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cp src/config.js.example src/config.js
-sed -i -e 's/5432/5436/' -e 's/\/var\/run\/postgresql/localhost/' -e 's/user: "cadastre"/user: "postgres"/' src/config.js
+sed -i -e 's/5432/5436/' -e 's/\/var\/run\/postgresql/localhost/' -e 's/user: "katastrio"/user: "postgres"/' src/config.js
 
-psql -p 5436 -c "CREATE DATABASE cadastre"
-psql -p 5436 -d cadastre -c "CREATE SCHEMA c_install"
+psql -p 5436 -c "CREATE DATABASE katastrio"
+psql -p 5436 -d katastrio -c "CREATE SCHEMA katastrio"
 
 wget -q -O liquibase.tar.gz https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.5.3/liquibase-3.5.3-bin.tar.gz
 wget -q -O postgresql94-jdbc.jar https://jdbc.postgresql.org/download/postgresql-9.4.1212.jre6.jar
@@ -14,7 +14,7 @@ mkdir liquibase
 
 tar -xzf liquibase.tar.gz -C liquibase
 
-find ./database -type f -path *.properties -exec sed -i -e 's/username: c_install/username: postgres/g' {} \; -exec sed -i -e 's/5432/5436/g' {} \; -exec sed -i -e '1d' {} \;
+find ./database -type f -path *.properties -exec sed -i -e 's/username: katastrio/username: postgres/g' {} \; -exec sed -i -e 's/5432/5436/g' {} \; -exec sed -i -e '1d' {} \;
 
 # liquibase
 cd ~/clone/database/database/etc/example
@@ -30,6 +30,6 @@ cd ~/clone/database/api_monitor/etc/example
 cd ~/clone/
 
 tar -xzf src/tests/share/dump.sql.tar.gz
-psql -p 5436 -d cadastre -f dump.sql
+psql -p 5436 -d katastrio -f dump.sql
 
 npm install
