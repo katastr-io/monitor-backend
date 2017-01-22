6825c24a181ced8fa5671a7abe001b70477c74ee
Michal Zimmermann <zimmicz@gmail.com>
2017-01-22 13:20:04 +0100

    Fix #1

--- src/routes/dates.js
+++ src/routes/dates.js
@@ -1,7 +1,7 @@
 import query from "../common/database";
 
 export default function (req, res, next) {
-    return query("SELECT DISTINCT valid_at FROM api_monitor.region_statistics")
+    return query("SELECT DISTINCT valid_at::text FROM api_monitor.region_statistics ORDER BY valid_at")
         .then((data) => {
             res.send(data.rows);
             return next();
