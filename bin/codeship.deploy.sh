911e698329ac710d7e9054dc5212543a0af7f09e
Michal Zimmermann <zimmicz@gmail.com>
2017-01-25 19:01:20 +0100

    Restart pm2 after deployment

--- bin/codeship.deploy.sh
+++ bin/codeship.deploy.sh
@@ -8,4 +8,10 @@ cd ~/clone
 npm install && \
 npm run build && \
 rsync -aqz ~/clone/dist/* codeship@193.85.199.37:${HOMEDIR}/monitor-backend-${COMMIT} && \
-ssh codeship@193.85.199.37 "find ${HOMEDIR} -maxdepth 1 -type d ! -name monitor-backend-${COMMIT} -name 'monitor-backend-*' -exec rm -rf '{}' \;"
+ssh codeship@193.85.199.37 << EOF 
+find ${HOMEDIR} -maxdepth 1 -type d ! -name monitor-backend-${COMMIT} -name 'monitor-backend-*' -exec rm -rf '{}' \;
+pm2 stop monitor-backend && \
+pm2 delete monitor-backend && \
+pm2 start ${HOMEDIR}/monitor-backend-${COMMIT}/src/server.js --name "monitor-backend" --watch
+EOF
+
